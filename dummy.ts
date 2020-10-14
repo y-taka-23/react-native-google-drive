9437b432346f5c7b5ca6b14e1ce58ee036d18425
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2020-10-15 01:56:37 +0900

    List dummy files

---
+++ dummy.ts
@@ -0,0 +1,87 @@
+export type DummyFile = {
+  id: string,
+  name: string,
+};
+
+export const dummyFiles: DummyFile[] = [
+  {
+    id: '001',
+    name: 'Lorem ipsum dolor',
+  },
+  {
+    id: '002',
+    name: 'consectetur adipiscing elit',
+  },
+  {
+    id: '003',
+    name: 'sed do eiusmod',
+  },
+  {
+    id: '004',
+    name: 'tempor incididunt',
+  },
+  {
+    id: '005',
+    name: 'ut labore et dolore',
+  },
+  {
+    id: '006',
+    name: 'magna aliqua',
+  },
+  {
+    id: '007',
+    name: 'ad minim veniam',
+  },
+  {
+    id: '008',
+    name: 'exercitation',
+  },
+  {
+    id: '009',
+    name: 'ullamco laboris',
+  },
+  {
+    id: '010',
+    name: 'aliquip ex ea commodo',
+  },
+  {
+    id: '011',
+    name: 'Lorem ipsum dolor',
+  },
+  {
+    id: '012',
+    name: 'consectetur adipiscing elit',
+  },
+  {
+    id: '013',
+    name: 'sed do eiusmod',
+  },
+  {
+    id: '014',
+    name: 'tempor incididunt',
+  },
+  {
+    id: '015',
+    name: 'ut labore et dolore',
+  },
+  {
+    id: '016',
+    name: 'magna aliqua',
+  },
+  {
+    id: '017',
+    name: 'ad minim veniam',
+  },
+  {
+    id: '018',
+    name: 'exercitation',
+  },
+  {
+    id: '019',
+    name: 'ullamco laboris',
+  },
+  {
+    id: '020',
+    name: 'aliquip ex ea commodo',
+  },
+];
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text, Button } from 'react-native';
+import { FlatList } from 'react-native';
+import { Icon, ListItem } from 'react-native-elements';
 import { useSelector, useDispatch } from 'react-redux';
 import { logInAsync } from 'expo-google-app-auth';
+import axios from 'axios';
 import { State, signIn } from '../store/actions';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { dummyFiles, DummyFile } from '../../dummy';
 
 type Props = {};
 
@@ -19,29 +13,23 @@ export const HomeScreen: React.FC<Props> = () => {
   const dispatch = useDispatch();
   const auth = useSelector((state: State) => state);
 
-  const handleSignIn = async () => {
-    try {
-      const result = await logInAsync({
-        androidClientId: ANDROID_CLIENT_ID,
-        iosClientId: IOS_CLIENT_ID,
-        scopes: ['profile', 'email'],
-      });
-      if (result.type === 'success') {
-        dispatch(signIn({username: result.user.name}));
-      } else {
-        return { cancelled: true };
-      }
-    } catch (err) {
-      return { error: true };
-    }
-  };
+  const extractFileKey = (file: DummyFile) => (file.id);
+
+  const renderFileItem = ({ item }: { item: DummyFile }) => (
+    <ListItem bottomDivider>
+      <Icon name='code' />
+      <ListItem.Content>
+        <ListItem.Title>{item.name}</ListItem.Title>
+      </ListItem.Content>
+     <ListItem.Chevron />
+    </ListItem>
+  );
 
   return (
-    <SafeAreaView style={styles.container}>
-      {auth.username
-        ? <Text>Signed in as {auth.username}!</Text>
-        : <Button title="SignIn with Google" onPress={handleSignIn} />
-      }
-    </SafeAreaView>
+    <FlatList
+      data={dummyFiles}
+      keyExtractor={extractFileKey}
+      renderItem={renderFileItem}
+    />
   );
 };
